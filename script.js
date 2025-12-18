92224962619cdcc8fcbce57a982523e422428631
Just-Winging-It <166638270+Just-Winging-It@users.noreply.github.com>
2025-12-17 21:59:22 -0500

    meme fix

--- script.js
+++ script.js
@@ -1407,8 +1407,8 @@ function startRandomPopups() {
         'https://i.imgflip.com/af5v8v.jpg',  // is this fair use
         'https://i.imgflip.com/af5vfq.jpg',  // They don't know
         'https://i.imgflip.com/af5vvj.jpg',   // Draw 25
-        'https://imgflip.com/i/af9gyv.jpg', // Protecting child
-        'https://imgflip.com/i/af9hfi.jpg' // Push both
+        'https://i.imgflip.com/af9gyv.jpg', // Protecting child
+        'https://i.imgflip.com/af9hfi.jpg' // Push both
     ];
     
     function createPopup() {
